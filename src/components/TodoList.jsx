8a03e760011171e0e5f67f4526d523c75d727d56
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-08 20:26:14 +0200

    add handleInputChange, addTask

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+
 const TodoList = () => {
+  const [tasks, setTasks] = useState([
+    "Write project",
+    "Read book",
+    "Watch Movie",
+  ]);
+  const [newTask, setNewTask] = useState("");
+  // const [task, setTask] = useState(["Write project", "Read book", "Watch Movie"]);
+  // const addTask = () => {
+  //   setTask([...task, newTask]);
+  //   console.log
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+    console.log(newTask);
+  };
+
+  const addTask = () => {
+    setTasks([...tasks, newTask]);
+  };
   return (
     <div className="px-8 flex flex-col justify-center items-center">
       <header>
@@ -7,14 +28,18 @@ const TodoList = () => {
       <main className="flex flex-col justify-center items-center">
         <h2 className="text-[28px] mb-6">Todo list:</h2>
         <div className="border rounded-xl p-4 mb-4">
-          <label htmlFor="todo"></label>
-          <input
-            type="text"
-            id="todo"
-            name="todo"
-            placeholder="write your task :"
-            className="h-[35px] p-2"
-          />
+          <form>
+            <label htmlFor="todo"></label>
+            <input
+              type="text"
+              id="todo"
+              name="todo"
+              value={newTask}
+              onChange={handleInputChange}
+              placeholder="write your task :"
+              className="h-[35px] p-2"
+            />
+          </form>
         </div>
         <ul className="border rounded-xl p-8 flex flex-col gap-4">
           <li className="border rounded-4xl p-4">
@@ -27,6 +52,12 @@ const TodoList = () => {
             <p>Watch Movie</p>
           </li>
         </ul>
+        <h2>List Demo</h2>
+        <ol className="list-decimal">
+          {tasks.map((task, index) => {
+            return <li key={index}>{task}</li>;
+          })}
+        </ol>
       </main>
       <footer></footer>
     </div>
