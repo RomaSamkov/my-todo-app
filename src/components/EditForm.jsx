d2ba8e8df21f8634dc2a0e64a8717f9a832adf6c
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-12 14:34:40 +0200

    Update EditForm.jsx

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,5 +1,35 @@
-const EditForm = () => {
-  return <div>EditForm</div>;
+import PropTypes from "prop-types";
+
+const EditForm = ({ editTodo, setEditTodo, editingTodo, index }) => {
+  return (
+    <div className="border rounded-xl p-4 mb-4 flex justify-center items-center">
+      <label htmlFor="todo"></label>
+      <input
+        type="text"
+        id="todo"
+        name="todo"
+        value={editTodo}
+        onChange={(e) => setEditTodo(e.target.value)}
+        placeholder="Edit your task :"
+        className="h-[35px] p-2"
+      />
+      <button className="cursor-pointer" onClick={() => editingTodo(index)}>
+        <div className="border rounded-full">
+          <img
+            src="/save_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg"
+            alt="add"
+          />
+        </div>
+      </button>
+    </div>
+  );
+};
+
+EditForm.propTypes = {
+  editTodo: PropTypes.string,
+  setEditTodo: PropTypes.func,
+  editingTodo: PropTypes.func,
+  index: PropTypes.number,
 };
 
 export default EditForm;
