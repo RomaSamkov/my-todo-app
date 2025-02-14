c319ad2c3c8be34599dd25c0275ee8434fc1c7cc
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-14 18:09:22 +0200

    add handleKeyDown

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,8 +1,31 @@
 import PropTypes from "prop-types";
+import { useEffect, useRef } from "react";
 
 const InputForm = ({ newTodo, setNewTodo, addTodo }) => {
+  const inputRef = useRef(null);
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (inputRef.current && !inputRef.current.contains(e.target)) {
+        setNewTodo("");
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [setNewTodo]);
   return (
-    <div className="border rounded-xl p-4 mb-4 flex justify-center items-center">
+    <div
+      className="border rounded-xl p-4 mb-4 flex justify-center items-center"
+      ref={inputRef}
+    >
       <label htmlFor="todo"></label>
       <input
         type="text"
@@ -10,6 +33,7 @@ const InputForm = ({ newTodo, setNewTodo, addTodo }) => {
         name="todo"
         value={newTodo}
         onChange={(e) => setNewTodo(e.target.value)}
+        onKeyDown={handleKeyDown}
         placeholder="write your task :"
         className="h-[35px] p-2"
       />
