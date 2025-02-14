d370096c52cedd1c1aec6102ae1dac1b5de62dfa
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-14 17:24:24 +0200

    add checkbox style

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -66,17 +66,55 @@ const TodoList = () => {
                     index={index}
                   />
                 ) : (
-                  <div className="flex gap-6">
-                    <p
-                      className={`break-all ${
-                        task.checked ? "line-through" : ""
+                  <div className="flex items-center">
+                    <label htmlFor="checkbox"></label>
+                    <input
+                      type="checkbox"
+                      name=""
+                      id="checkbox"
+                      checked={task.checked}
+                      onChange={() => handleCheckbox(index)}
+                      className="relative peer shrink-0
+                      appearance-none w-4 h-4 border-2 border-blue-500 rounded-sm bg-white
+                      mt-1
+                      checked:bg-blue-800 checked:border-0
+                      focus:outline-none focus:ring-offset-0 focus:ring-2 focus:ring-blue-100
+                      disabled:border-steel-400 disabled:bg-steel-400"
+                    />
+                    <svg
+                      className="
+                      absolute
+                      w-4 h-4 mt-1
+                      hidden peer-checked:block
+                      pointer-events-none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <polyline points="20 6 9 17 4 12"></polyline>
+                    </svg>
+                    <div
+                      className={`flex gap-6 ml-2 ${
+                        task.checked
+                          ? "line-through bg-green-600 rounded-2xl"
+                          : ""
                       }`}
                     >
-                      {task.text}
-                    </p>
+                      <p
+                        className={`break-all rounded-2xl px-4 ${
+                          task.checked ? "line-throug" : ""
+                        }`}
+                      >
+                        {task.text}
+                      </p>
+                    </div>
                   </div>
                 )}
-                <div className="flex justify-end gap-6">
+                <div className="flex justify-end gap-2">
                   {editingIndex !== index && (
                     <button
                       className="cursor-pointer"
@@ -104,13 +142,6 @@ const TodoList = () => {
                       />
                     </div>
                   </button>
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={task.checked}
-                    onChange={() => handleCheckbox(index)}
-                  />
                 </div>
               </li>
             );
